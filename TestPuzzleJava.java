4304dbe7dba0222b557fd702f7fab6fd26ea9e72
Azachary8232 <90228565+Azachary8232@users.noreply.github.com>
2021-11-29 13:53:56 -0700

    Puzzling Setup

---
+++ TestPuzzleJava.java
@@ -0,0 +1,18 @@
+import java.util.ArrayList;
+import java.util.Random;
+
+
+
+
+public class TestPuzzleJava{
+    
+    public class void main(String[] args) {
+            PuzzleJava generator = new PuzzleJava();
+            ArrayList<Integer> randomRolls = generator.getTenRolls();
+            System.out.println(randomRolls);
+        
+    }
+
+
+
+}
